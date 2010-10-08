4979c66b939e28c94b939eb4a9559e36f772d063
Linus Oleander <linus@oleander.nu>
2010-10-09 00:30:00 +0200

    Gör klar uppg 3

--- BasicGate.java
+++ BasicGate.java
@@ -1,9 +1,37 @@
-public abstract class BasicGate extends Gate {
+import javax.swing.Timer;
+import java.awt.event.*;
+import java.util.*;
+
+public abstract class BasicGate extends Gate implements ActionListener {
+  private Timer timer = new Timer(delay,this);
+  private boolean currentCalc;
+  
   public BasicGate(){
     
   }
   
   public void inputChanged(){
-    this.outputChanged(this.calculateValue());
+    if(!this.timer.isRunning()){
+      this.currentCalc = this.calculateValue();
+      
+      /* Är det nya värdet skilt från det nuvarande ? */
+      if(this.getOutputValue() != this.currentCalc){
+        this.timer.start();
+      }
+      
+    }
+  }
+  
+  public void actionPerformed(ActionEvent e){
+    /* Är det verkligen ett timer-objekt som har kickat igång detta callback ? */
+    if(!(e.getSource() instanceof Timer)){
+      return;
+    }
+    /* Sparar undan värdet 
+       Gör dock inte beräkningen igen, använder {currentCalc} */
+    this.outputChanged(this.currentCalc);
+    
+    /* Stoppar timern, så att denna metod inte körs igenom om {delay} ms */
+    ((Timer) e.getSource()).stop();
   }
 }
